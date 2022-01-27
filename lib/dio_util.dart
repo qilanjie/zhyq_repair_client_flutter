7b13778a4e7025dfcb425f14c6ef279b126f0604
qiwenhua <qilanjie@126.com>
2022-01-27 09:35:53 +0800

    Signed-off-by: qiwenhua <qilanjie@126.com>

---
+++ lib/dio_util.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DioUtil{
+  // 单例模式
+  static final DioUtil _instance = DioUtil._internal();
+  factory DioUtil() => _instance;
+  DioUtil._internal(){
+    init();
+  }
+
+ late  Dio _dio;
+
+  // 初始化请求配置
+  init()  async {
+   SharedPreferences prefs = await SharedPreferences.getInstance();
+    BaseOptions baseOptions = BaseOptions(
+      baseUrl: "http://192.168.203.67:8888/api/private/v1/",
+      headers:{"token":prefs.getString('token')},
+      connectTimeout: 5000,
+    );
+    _dio = Dio(baseOptions);
+  }
+
+  // 请求(默认post)
+  Future dio_requset(String url,{String method = "post", Map<String,dynamic>? params}) async{
+    Options options = Options(method: method);
+    try{
+      final result = await _dio.request(url,queryParameters: params,options: options);
+      return result;
+    } on DioError catch(error){
+      throw error;
+    }
+  }
+}
